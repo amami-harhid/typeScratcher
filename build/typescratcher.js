ccc302ed1ed4a153a1b5b919d0134e4a311941af
amami-harhid <haranaga.hidehiro@itimes.co.jp>
2026-08-19 11:23:13 +0900

    0.1.7

--- build/typescratcher.js
+++ build/typescratcher.js
@@ -15022,7 +15022,7 @@ function wAA() {
   return nP || (nP = 1, rl = EAA()), rl;
 }
 wAA();
-const uAA = "0.1.6", lAA = {
+const uAA = "0.1.7", lAA = {
   version: uAA
 }, IAA = lAA.version, Dt = {
   main_id: "main",
